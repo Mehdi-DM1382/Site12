7f7cc873000d20f2d30dfad71b55fb914d41e779
MehdiDM <Mehdidm1382@gmail.com>
2025-05-31 15:09:48 +0430

    firstc

---
+++ Mehdidoostmoammadiapp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mehdidoostmoammadiapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32( Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+b);
+            Console.ReadKey();
+            Console.ReadKey();
+
+        }
+    }
+}
